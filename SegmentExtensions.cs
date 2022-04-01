2db1c445918f4151144de5188d757e63a0f798ce
Akulko_O?! <57037465+Akulko71@users.noreply.github.com>
2022-04-01 15:17:51 +0300

    Добавьте файлы проекта.

---
+++ SegmentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+
+namespace GeometryPainting
+{
+    static class SegmentExtension
+    {
+        public static Color GetColor(this Segment a)
+        {
+            return Color.Green;
+        }
+        public static Color SetColor(this Segment a, Color color)
+        {
+            return color;
+        }
+    }
+}
+
